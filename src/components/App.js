f230a0997b5a9beaab2bf93d7c8917a1aefa75bd
Satoshi Scott Iwako <iwakoscott@gmail.com>
2018-03-22 12:44:08 -0700

    build homepage with Links to each team using SVG icons

--- src/components/App.js
+++ src/components/App.js
@@ -1,5 +1,5 @@
 import React, { Component } from 'react';
-import { BrowserRouter as Router, Route } from 'react-router-dom';
+import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
 import Home from './Home';
 import Teams from './Teams';
 import Players from './Players';
@@ -12,9 +12,12 @@ class App extends Component {
       <Router>
         <div>
           <Navbar />
-          <Route exact path="/" component={Home}/>
-          <Route path="/players" component={Players} />
-          <Route path="/teams" component={Teams} />
+          <Switch>
+            <Route exact path="/" component={Home}/>
+            <Route path="/players" component={Players} />
+            <Route path="/teams" component={Teams} />
+            <Route render={() => (<h1 className="text-center">404 Error</h1>)} />
+          </Switch>
         </div>
       </Router>
     );
--- src/components/Home.js
+++ src/components/Home.js
@@ -1,10 +1,35 @@
 import React, { Component } from 'react';
+import { Link } from 'react-router-dom';
+import TeamLogo from './TeamLogo';
+import { getTeamNames } from '../api';
 
 
 class Home extends Component {
+  state = {
+    teamNames: []
+  }
+
+  componentDidMount(){
+    getTeamNames().then(teamNames => this.setState(() => ({ teamNames })));
+  }
+
   render(){
     return (
-      <div className="container">Home</div>
+      <div className="container">
+        <h1 className="large-header">Hash History Basketball League</h1>
+        <h3 className="header text-center">Select a team</h3>
+
+        <div className="home-grid">
+          {this.state.teamNames.map(id => (
+            <Link
+              key={id}
+              to={`/${id}`}
+              >
+              <TeamLogo id={id} width={125}/>
+            </Link>
+          ))}
+        </div>
+      </div>
     );
   } // render
 }
