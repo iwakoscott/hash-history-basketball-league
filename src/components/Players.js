a27f89abba610ffc6cadd0ae58b85d80c49126f1
Satoshi Scott Iwako <iwakoscott@gmail.com>
2018-03-22 12:19:51 -0700

    add boiler plate for Home, Teams, Players components

---
+++ src/components/Players.js
@@ -0,0 +1,12 @@
+import React, { Component } from 'react';
+
+
+class Players extends Component {
+  render(){
+    return (
+      <div className="container two-column">Players</div>
+    );
+  } // render
+}
+
+export default Players;
---
+++ src/components/Teams.js
@@ -0,0 +1,12 @@
+import React, { Component } from 'react';
+
+
+class Teams extends Component {
+  render(){
+    return (
+      <div className="container two-column">Teams</div>
+    );
+  } // render
+}
+
+export default Teams;
